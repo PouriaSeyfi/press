efb474bdeec4b1c12be0c96da743d20b943f9ff7
Pouria Seyfi <seyfipouria@gmail.com>
2021-06-04 13:02:10 +0430

    add testbench package
    first phpunit test

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace pouria\Press\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+
+    public function test_my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
