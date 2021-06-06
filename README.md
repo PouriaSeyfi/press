619cfd49aa10fee7b2ebe36e36fa38ee0c3cc5f6
Pouria Seyfi <seyfipouria@gmail.com>
2021-06-07 00:52:59 +0430

    allowing for field expansion of posts part 3

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-### Lesson 25
+### Lesson 26
 
-#### Allowing for field expansion of posts part 2
+#### Allowing for field expansion of posts part 3
--- src/Press.php
+++ src/Press.php
@@ -32,7 +32,7 @@ class Press
 
     public function availableFields()
     {
-        return $this->fields;
+        return array_reverse($this->fields);
     }
 
 }
